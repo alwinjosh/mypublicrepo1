f6e6543a724710434cc40035fcfddeacc2d55fdb
Alwin <alwjoseph@paypal.com>
2013-03-11 15:31:38 +0530

    this is branch changes

--- testgit/src/main/java/me/alwin/git/sample/GitTest.java
+++ testgit/src/main/java/me/alwin/git/sample/GitTest.java
@@ -3,5 +3,6 @@ package me.alwin.git.sample;
 public class GitTest {
 	  public static void main(String[] args) {
 	    System.out.println("Git is cool");
+	    System.out.println("This is branch changes");
 	  }
 	}
